dd0948b815980f32a23638ffa7557f6ef497e487
LucasMigliaccio <lucasmigliaccio10@gmail.com>
2022-07-12 18:54:47 -0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from datetime import date
+from django.db import models
+import datetime
+
+
+# Create your models here.
+
+class Posts(models.Model):
+    tittle=models.CharField(max_length=100)
+    description=models.TextField()
+    image=models.ImageField(upload_to="blog/images")
+    date=models.DateField(datetime.date.today)
+
---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Posts
+# Create your views here.
+def render_posts(request):
+    post= Posts.objects.all()
+    return render(request, 'post.html',{"posts":post})
+
+def post_detail(request, post_id):
+    posts=get_object_or_404(Posts, pk=post_id)
+    return render(request, "post_detail.html", {"post":posts})
