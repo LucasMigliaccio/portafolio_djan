284d559e0d2d54e509467520b6d005c3516b0756
LucasMigliaccio <97121582+LucasMigliaccio@users.noreply.github.com>
2022-08-18 13:33:49 -0300

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -129,7 +129,7 @@ EMAIL_HOST= "smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT= 587
 EMAIL_HOST_USER= "devmigliaccio@gmail.com"
-EMAIL_HOST_PASSWORD="lucasprogramadorDjango1"
+EMAIL_HOST_PASSWORD="/YOUR PASSWORD/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
